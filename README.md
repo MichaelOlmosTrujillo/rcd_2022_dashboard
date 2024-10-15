aead4b3824d2a8362309ef859c6d786a8e604721
MichaelOlmosTrujillo <molmost@unal.edu.co>
2024-10-15 21:07:59 +0000

    agregando gestores

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # :Reporte de Residuos de Construcción y Demolición (RCD) en Colombia - 2022: Reporte anual
 
-Es un reporte que busca mostrar la problemática ambiental de los RCD en el país.
+* Es un reporte que busca mostrar la problemática ambiental de los RCD en el país.
+* Permite observar los gestores de RCD en un mapa observando sus característcas principales.
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 import altair as alt
 import plotly.express as px
+import pydeck as pdk
 # from millify import millify
 
 # configuración página
@@ -14,20 +15,37 @@ st.set_page_config(
 
 alt.themes.enable("dark")
 ruta = './data/indicadores_por_AA.xlsx'
-ruta_gestores = './datos/Base de datos final_v2_maria_paula.xlsx'
+ruta_gestores = './data/gestores_coordenadas.csv'
 df = pd.read_excel(ruta, usecols=lambda column: column != 'Unnamed: 0')
 cantidad_autoridades_ambientales = df.shape[0]
-# df_gestores = pd.read_excel(ruta_gestores, 
-#                             usecols=lambda column: column!= 'Unnamed: 0',
-#                             sheet_name='gestores')
-# df_coord_gestores = df_gestores[['latitude', 'longitude']]
-
+df_gestores = pd.read_csv(ruta_gestores, 
+                            usecols=lambda column: column!= 'Unnamed: 0',
+                            sep = ',')
+# st.dataframe(df_gestores)
+# st.write(df_gestores.columns)
+df_coord_gestores = df_gestores[['latitude', 'longitude']]
+# st.dataframe(df_coord_gestores)
 ### título
 titulo = '<p style="font-family:sans-serif; color:Green; font-size: 24px;'
 titulo += 'text-align: center">'
 titulo += 'Residuos de Construcción y Demolición (RCD) reportados por las '
 titulo += 'Autoridades Ambientales - 2022</p>'
-st.markdown(titulo, unsafe_allow_html=True)
+
+ruta_logo = './images/logo_minambiente.png'
+st.logo(ruta_logo, size = 'medium')
+
+col_titulo_1, col_titulo_2 = st.columns([0.2, 0.8])
+with col_titulo_1:
+    st.image(ruta_logo)
+with col_titulo_2:
+    st.markdown(titulo, unsafe_allow_html=True)
+## Menú
+menu = option_menu(None, ["RCD 2022", "Mapa de gestores"],
+                   icons = ['house', 'geo-alt-fill'],
+                   menu_icon = 'cast',
+                   default_index=0,
+                   orientation='horizontal')
+menu
 # Crear sidebar para seleccionar una autoridad ambiental
 df.rename(columns = {
     'autoridad_ambiental': 'Autoridad Ambiental',
@@ -41,32 +59,20 @@ df.rename(columns = {
     }, inplace = True
 )
 
-with st.sidebar:
-    st.title('RCD en Colombia para el año 2022')
-    autoridad_ambiental_lista = list(df['Autoridad Ambiental'].unique())
-    selected_autoridades = st.multiselect('Selecciona una Autoridad Ambiental',
-                   autoridad_ambiental_lista,
-                   )
-# with st.sidebar:
-#     selected = option_menu("RCD", ["RCD 2022", "mapa de gestores"],
-#                            icons = ['house', 'globe-americas'], menu_icon = 'cast',
-#                            default_index = 1)
-#     selected
-### Mapeo de gestores
-# st.dataframe(df_coord_gestores)
-# st.map(df_coord_gestores, 
-#        latitude = 'latitude',
-#        longitude = 'longitude')
-### Datos Autoridades Ambientales RCD 2022
-## gráfico meta de aprovechamiento
-df_meta_aprov = df[['Autoridad Ambiental', 'meta_de_aprovechamiento']]
-if len(selected_autoridades) > 0:
-    df = df[df['Autoridad Ambiental'].isin(selected_autoridades)]
+if menu == "RCD 2022":
+    
 
-# st.dataframe(df)
-### 
-# Seleccionando columnas
-columnas = ['Autoridad Ambiental', 
+    with st.sidebar:
+        st.title('RCD en Colombia para el año 2022')
+        autoridad_ambiental_lista = list(df['Autoridad Ambiental'].unique())
+        selected_autoridades = st.multiselect('Selecciona una Autoridad Ambiental',
+                    autoridad_ambiental_lista,
+                    )
+    df_meta_aprov = df[['Autoridad Ambiental', 'meta_de_aprovechamiento']]
+    if len(selected_autoridades) > 0:
+        df = df[df['Autoridad Ambiental'].isin(selected_autoridades)]
+
+    columnas = ['Autoridad Ambiental', 
             'RCD Generado',
             # 'indicador_cantidad_de_rcd_aprovechado', 
             'RCD Aprovechado en Obra',
@@ -75,75 +81,131 @@ columnas = ['Autoridad Ambiental',
             'RCD enviado a Punto Limpio', 
             'RCD enviado a Receptor'  
             ]
-df_barplot = df[columnas]
+    df_barplot = df[columnas]
 
-    # st.dataframe(df)
-### 
-# Melt the DataFrame to a long format
-df_melted = df_barplot.melt(id_vars='Autoridad Ambiental', 
-                    var_name='Tipo de RCD', 
-                    value_name='RCD en toneladas')
+        # st.dataframe(df)
+    ### 
+    # Melt the DataFrame to a long format
+    df_melted = df_barplot.melt(id_vars='Autoridad Ambiental', 
+                        var_name='Tipo de RCD', 
+                        value_name='RCD en toneladas')
 
-# plotly
-fig = px.bar(df_melted, x = 'Autoridad Ambiental', y = 'RCD en toneladas',
-             color = 'Tipo de RCD', barmode = 'group',
-             width=800, height = 500
-             )
-fig.update_layout(barmode = 'group', bargroupgap = 0.1, xaxis_tickangle = -45)
-fig.update_traces(width = 0.2)
-st.plotly_chart(fig)
+    # plotly
+    fig = px.bar(df_melted, x = 'Autoridad Ambiental', y = 'RCD en toneladas',
+                color = 'Tipo de RCD', barmode = 'group',
+                width=800, height = 500
+                )
+    fig.update_layout(barmode = 'group', bargroupgap = 0.1, xaxis_tickangle = -45)
+    fig.update_traces(width = 0.2)
+    st.plotly_chart(fig)
 
-### Gráfico de los indicadores
-media_meta_aprov = df['meta_de_aprovechamiento'].mean()
-# rcd_generado = millify(df['RCD Generado'].sum())
-rcd_generado = round(df['RCD Generado'].sum(), 1)
-# rcd_dispo_final = millify(df['RCD enviado a Disposición Final'].sum())
-rcd_dispo_final = round(df['RCD enviado a Disposición Final'].sum(), 1)
-label_meta_aprov = "Promedio de las metas de aprovechamiento: "
+    ### Gráfico de los indicadores
+    media_meta_aprov = df['meta_de_aprovechamiento'].mean()
+    # rcd_generado = millify(df['RCD Generado'].sum())
+    rcd_generado = round(df['RCD Generado'].sum(), 1)
+    # rcd_dispo_final = millify(df['RCD enviado a Disposición Final'].sum())
+    rcd_dispo_final = round(df['RCD enviado a Disposición Final'].sum(), 1)
+    label_meta_aprov = "Promedio de las metas de aprovechamiento: "
 
-# Create an indicator for each row in the DataFrame
-col_1, col_2, col_3 = st.columns(3)
-if df.shape[0] == cantidad_autoridades_ambientales:
-    with col_1:
-        label_meta_aprov = "Promedio de las metas de aprovechamiento"
-        st.metric(label = label_meta_aprov,
-                value = f"{media_meta_aprov * 100:.1f}%",
-                delta='Excluyendo material de excavación',
-                delta_color='inverse')
-    with col_2:
-        label_gen = 'Total de RCD generado'
-        st.metric(label = label_gen,
-                value = f"{rcd_generado:,.0f}",
-                delta='Toneladas')
-    with col_3:
-        label_dispo_final = 'Total de RCD enviado a Disposición Final'
-        st.metric(label = label_dispo_final, 
-                  value = f'{rcd_dispo_final:,.0f}',
-                  delta='Toneladas',
-                  delta_color='normal')
-else:
-    for i, row in df.iterrows():
+    # Create an indicator for each row in the DataFrame
+    col_1, col_2, col_3 = st.columns(3)
+    if df.shape[0] == cantidad_autoridades_ambientales:
         with col_1:
-            label_meta_aprov = "Meta de aprovechamiento: "
-            st.metric(label= label_meta_aprov + f"{row['Autoridad Ambiental']}",
-                    value=f"{row['meta_de_aprovechamiento'] * 100:.1f}%",
+            label_meta_aprov = "Promedio de las metas de aprovechamiento"
+            st.metric(label = label_meta_aprov,
+                    value = f"{media_meta_aprov * 100:.1f}%",
                     delta='Excluyendo material de excavación',
                     delta_color='inverse')
         with col_2:
-            label_gen = "RCD generado: "
-            # rcd_gen = millify(row['RCD Generado'])
-            rcd_gen = round(row['RCD Generado'], 1)
-            st.metric(label= label_gen + f"{row['Autoridad Ambiental']}",
-                    value=f"{rcd_gen:,.0f}",
+            label_gen = 'Total de RCD generado'
+            st.metric(label = label_gen,
+                    value = f"{rcd_generado:,.0f}",
                     delta='Toneladas')
         with col_3:
-            label_dispo_final = "RCD enviado a Disposición Final: "
-            # rcd_dispo_final = millify(row['RCD enviado a Disposición Final'])
-            rcd_dispo_final = round(row['RCD enviado a Disposición Final'], 0)
-            st.metric(label=label_dispo_final + f"{row['Autoridad Ambiental']}",
-                    value=f'{rcd_dispo_final:,.0f}',
+            label_dispo_final = 'Total de RCD enviado a Disposición Final'
+            st.metric(label = label_dispo_final, 
+                    value = f'{rcd_dispo_final:,.0f}',
                     delta='Toneladas',
                     delta_color='normal')
+    else:
+        for i, row in df.iterrows():
+            with col_1:
+                label_meta_aprov = "Meta de aprovechamiento: "
+                st.metric(label= label_meta_aprov + f"{row['Autoridad Ambiental']}",
+                        value=f"{row['meta_de_aprovechamiento'] * 100:.1f}%",
+                        delta='Excluyendo material de excavación',
+                        delta_color='inverse')
+            with col_2:
+                label_gen = "RCD generado: "
+                # rcd_gen = millify(row['RCD Generado'])
+                rcd_gen = round(row['RCD Generado'], 1)
+                st.metric(label= label_gen + f"{row['Autoridad Ambiental']}",
+                        value=f"{rcd_gen:,.0f}",
+                        delta='Toneladas')
+            with col_3:
+                label_dispo_final = "RCD enviado a Disposición Final: "
+                # rcd_dispo_final = millify(row['RCD enviado a Disposición Final'])
+                rcd_dispo_final = round(row['RCD enviado a Disposición Final'], 0)
+                st.metric(label=label_dispo_final + f"{row['Autoridad Ambiental']}",
+                        value=f'{rcd_dispo_final:,.0f}',
+                        delta='Toneladas',
+                        delta_color='normal')
+elif menu == "Mapa de gestores":
+
+# with st.sidebar:
+#     selected = option_menu("RCD", ["RCD 2022", "mapa de gestores"],
+#                            icons = ['house', 'globe-americas'], menu_icon = 'cast',
+#                            default_index = 1)
+#     selected
+### Mapeo de gestores
+# st.dataframe(df_coord_gestores)
+    layer = pdk.Layer(
+        "ScatterplotLayer",
+        data = df_gestores,
+        id = "nomb",
+        get_position = ["longitude", "latitude"],
+        get_color="[12, 153, 7]",
+        pickable = True,
+        auto_highlight = True,
+        get_radius = 3000,
+    )
+    # Set the view of the map
+    view_state = pdk.ViewState(
+        latitude=df_gestores['latitude'].mean(),
+        longitude=df_gestores['longitude'].mean(),
+        controller = True,
+        zoom=5,
+        pitch=10
+    )
+    # Renderizando el mapa
+    texto_tooltip = "Nombre del gestor: {nomb}\n" 
+    texto_tooltip += "Actividad de recolección y transporte: {recolec}\n"
+    texto_tooltip += "Actividad de almacenamineto en punto limpio: {almac}\n"
+    texto_tooltip += "Actividad de aprovechamiento: {aprovec}\n"
+    texto_tooltip += "Actividad de disposición final: {finaldis}\n"
+    texto_tooltip += "Autoridad Ambiental: {aa}\n"
+
+    grafico = pdk.Deck(layers=[layer],
+                initial_view_state=view_state,
+                tooltip={'text':texto_tooltip},
+                )
+
+    evento = st.pydeck_chart(grafico, on_select="rerun", selection_mode='multi-object')
+    evento.selection
+
+# st.map(df_gestores[['latitude', 'longitude']], 
+#        latitude = 'latitude',
+#        longitude = 'longitude',
+#        color = '#0c9907',
+#        size = 200,
+#        zoom = 4)
+### Datos Autoridades Ambientales RCD 2022
+## gráfico meta de aprovechamiento
+
+# st.dataframe(df)
+### 
+# Seleccionando columnas
+
 # plot_meta_aprov = alt.Chart(df_meta_aprov).mark_arc(innerRadius=45, cornerRadius=25).encode(
 #       theta="meta_de_aprovechamiento",
 #     #   color= alt.Color("Topic:N",
